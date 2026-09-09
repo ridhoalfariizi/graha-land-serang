1578a56c35a822754ed502248d5aa2a5256834f8
ridhoalfariizi <168616852+ridhoalfariizi@users.noreply.github.com>
2026-09-09 22:26:32 +0000

    Fix typo in Railway environment variable fallback check

--- config/database.php
+++ config/database.php
@@ -15,8 +15,7 @@ return [
     | the connection which will be utilized unless another connection
     | is explicitly specified when you execute a query / statement.
     |
-    */
-    'default' => isset($_ENV['RAILWAY_ENVIRONMENT_NAME']) ? 'sqlite' : env('DB_CONNECTION', 'mysql'),
+    */    'default' => isset($_ENV['RAILWAY_ENVIRONMENT']) ? 'sqlite' : env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
